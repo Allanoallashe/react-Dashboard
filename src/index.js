d629a5afa8c5b539377b1749d93102a2b989c4c4
Allan Lenny <owinoallan77@gmail.com>
2023-02-04 00:20:20 +0300

    edited index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './src/scenes/App';
+import App from './scenes/App';
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
